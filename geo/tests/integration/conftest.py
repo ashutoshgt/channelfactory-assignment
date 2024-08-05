a423e1732306b28fbdbef49df24dd7e21d134ea3
Ashutosh Gupta <v-agupta@acvauctions.com>
2024-08-05 09:08:46 +0530

    removes unused fixture

--- geo/tests/integration/conftest.py
+++ geo/tests/integration/conftest.py
@@ -15,29 +15,5 @@ def invalid_geocode_response():
         yield mock
 
 @pytest.fixture
-def valid_geocode_response():
-    with Mocker() as mock:
-        # Mock the response for a successful geocode API call
-        mock.get(
-            "https://maps.googleapis.com/maps/api/geocode/json",
-            json={
-                "results": [
-                    {
-                        "formatted_address": "Formatted Address",
-                        "geometry": {
-                            "location": {
-                                "lat": 37.7749295,
-                                "lng": -122.4194155
-                            }
-                        }
-                    }
-                ],
-                "status": "OK"
-            },
-        )
-
-        yield mock
-
-@pytest.fixture
 def api_client():
     return APIClient()
